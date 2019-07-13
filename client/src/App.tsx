45d022adcf11052019596ee6381494a6f5869ea5
Laura Aubin <laura.aubin@shopify.com>
2019-07-13 17:05:01 -0400

    Detect when you click on non-applications from the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-// import React from 'react';
-import * as React from "react";
+import * as React from 'react';
+
+import './App.scss';
 
 interface State {
   readFile: string;
@@ -13,18 +14,29 @@ class App extends React.Component<{}, State> {
 
   componentDidMount() {
     this.readFile('./server.js');
+
+    document.addEventListener('mousedown', this.handleClick);
   }
 
   render() {
     const { readFile } = this.state;
 
     return (
+      <div id='mainWindow' className='MainWindow'>
       <div>
         File contents of server.js:
         <div>{readFile}</div>
       </div>
+        <div>Bottom</div>
+      </div>
     );
   }
+
+  handleClick = (event: any) => {
+    if (event.toElement.id == 'mainWindow') {
+      console.log('Clicked outside of content');
+    }
+  };
 
   fetchRequest = async (type: string, path: string, body: any) => {
     return await fetch(path, {
