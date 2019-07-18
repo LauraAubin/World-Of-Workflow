30c66eeec18bab62f4b238160fe873d0a5947773
Laura Aubin <laura.aubin@shopify.com>
2019-07-18 01:25:09 -0400

    Read from the database and pass in expressions

--- client/src/Utilities/Database.ts
+++ client/src/Utilities/Database.ts
@@ -1,13 +1,35 @@
 import { fetchRequest } from './Fetch';
 
+// const whereRegex = new RegExp('[a-z]+\s?[<>]?={2}?\s?[a-z]+');
+/*
+  Regex description:
+
+  One or more of any letter in this range: [a-z]+
+  Optional space: \s?
+  Optional of either of these symbols: [<>]?
+  Optional of 2 of the equals sign: ={2}?
+*/
+
 export async function readFile() {
-  return await fetchRequest('post', 'readFile', { file: './DB.js' }).then(
-    resp => resp.json()
-  );
+  return await fetchRequest('post', 'readFile', {}).then(resp => resp.json());
 }
 
 export async function writeTo(table: string, contents: { message: string }) {
   await fetchRequest('post', 'writeTo', { table, contents }).then(resp =>
+    resp.json()
+  );
+}
+
+export async function readFrom(table: string, where?: any) {
+  /*
+  if (!whereRegex.test(where))
+    return {
+      error:
+        'The "where" parameter is of the wrong format or includes an unknown condition'
+    };
+  */
+
+  return await fetchRequest('post', 'readFrom', { table, where }).then(resp =>
     resp.json()
   );
 }
--- server/server.js
+++ server/server.js
@@ -5,18 +5,31 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const OPERATORS = ['<', '>', '=='];
+const EXTENSIONS = ['and'];
+
 app.use(bodyParser.json());
 
 app.post('/readFile', (request, response) => {
-  const { file } = request.body;
-
-  response.send({ fileContents: readFile(file) });
+  response.send({ fileContents: readFile() });
 });
 
 app.post('/writeTo', (request, response) => {
   const { table, contents } = request.body;
 
   addNewRecord({ table, message: contents.message });
+});
+
+app.post('/readFrom', (request, response) => {
+  const { table, where } = request.body;
+
+  const filterByTable = filterRecordsByTable(table);
+
+  const filterByExpression = separateExpressions(where).map(expression =>
+    filterRecordsByExpression(filterByTable, expression)
+  );
+
+  response.send({ records: filterByExpression });
 });
 
 function readFile() {
@@ -31,13 +44,64 @@ function addNewRecord(contents) {
   const databaseContents = JSON.parse(readFile('DB.js'));
 
   databaseContents.push({ ...contents, created_at: new Date() });
-
   const updatedDB = addNewLines(JSON.stringify(databaseContents));
+
   writeFile(updatedDB);
 }
 
-function addNewLines(string) {
-  return string.split('},').join('},\n');
+function addNewLines(s) {
+  const commonSeparator = '},';
+
+  return s.split(commonSeparator).join(`${commonSeparator}\n`);
+}
+
+function filterRecordsByTable(table) {
+  const databaseContents = JSON.parse(readFile());
+
+  return databaseContents.filter(record => record.table == table);
+}
+
+function filterRecordsByExpression(records, expression) {
+  const operator = getOperator(expression);
+
+  const leftSide = record => record[leftSideOfExpression(expression, operator)];
+  const rightSide = rightSideOfExpression(expression, operator);
+
+  return records.filter(record =>
+    eval(`'${leftSide(record)}' ${operator} '${rightSide}'`)
+  );
+}
+
+function removeWhitespaces(s) {
+  // g means to repeat this search through the entire string
+  return s.replace(/ /g, '');
+}
+
+function separateExpressions(where) {
+  const query = removeWhitespaces(where);
+
+  return EXTENSIONS.map(extension => query.split(extension))[0];
+}
+
+function getOperator(expression) {
+  let result;
+
+  OPERATORS.map(operator => {
+    if (expression.includes(operator)) result = operator;
+  });
+
+  return result;
+}
+
+function leftSideOfExpression(expression, operator) {
+  return expression.substring(0, expression.indexOf(operator));
+}
+
+function rightSideOfExpression(expression, operator) {
+  return expression.substring(
+    expression.length,
+    expression.indexOf(operator) + operator.length
+  );
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
