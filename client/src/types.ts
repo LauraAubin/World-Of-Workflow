e358f772da77d35f250bf599307f2e545b0515d3
Laura Aubin <laura.aubin@shopify.com>
2019-07-25 18:40:17 -0400

    Create a Quest type

---
+++ client/src/types.ts
@@ -0,0 +1,10 @@
+export interface Quest {
+  table: string;
+  title: string;
+  location: string;
+  description: string;
+  questObjectives: string;
+  dueDate: string;
+  test: boolean;
+  created_at: Date;
+}
