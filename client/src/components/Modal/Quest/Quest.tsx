cd67a0d409f3d9fbae80fe2eaa50528b98b12774
Laura Aubin <laura.aubin@shopify.com>
2019-07-26 12:06:18 -0400

    View quest objectives

--- client/src/components/Modal/Quest/Quest.tsx
+++ client/src/components/Modal/Quest/Quest.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import { Quest as QuestType } from '../../../types';
 
+import './Quest.scss';
+
 interface Props {
   selectedQuest: QuestType;
 }
 
-export default function Quest({ selectedQuest }: Props) {
-  return selectedQuest && <div>{selectedQuest.title}</div>;
+export default function Quest({
+  selectedQuest: { title, questObjectives }
+}: Props) {
+  return (
+    <div className='QuestContainer'>
+      <div>{title}</div>
+      <div>{questObjectives}</div>
+    </div>
+  );
 }
