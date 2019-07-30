c5a1b79a45b944e650a24821dad3fcd9d1b0977b
Laura Aubin <laura.aubin@shopify.com>
2019-07-30 15:33:33 -0400

    Move overlay rendering logic into its own function

--- client/src/components/Image/Image.tsx
+++ client/src/components/Image/Image.tsx
@@ -13,33 +13,36 @@ interface Props {
 }
 
 export default function Image({ image, overlay }: Props) {
-  const x = overlay ? overlay.x : 0;
-  const y = overlay ? overlay.y : 0;
-
   const imageStyle = {
     height: `${image.height}.px`
   };
 
-  const overlayStyle = {
-    paddingLeft: x,
-    paddingTop: y
-  };
-
-  const overlayImageStyle = { height: `${overlay && overlay.height}px` };
-
-  const overlayElement =
-    typeof overlay!.element == 'string' ? (
-      <img src={overlay!.element} style={overlayImageStyle} />
-    ) : (
-      overlay!.element
-    );
-
   return (
     <div className='imageContainer'>
-      <div className='overlay' style={overlayStyle}>
-        {overlay && overlayElement}
-      </div>
+      {overlay && overlayElement(overlay)}
       <img src={image.element} style={imageStyle} />
+    </div>
+  );
+}
+
+function overlayElement(overlay) {
+  const imageStyle = { height: `${overlay.height}px` };
+
+  const element =
+    typeof overlay.element == 'string' ? (
+      <img src={overlay.element} style={imageStyle} />
+    ) : (
+      overlay.element
+    );
+
+  const overlayStyle = {
+    paddingLeft: overlay.x,
+    paddingTop: overlay.y
+  };
+
+  return (
+    <div className='overlay' style={overlayStyle}>
+      {element}
     </div>
   );
 }
