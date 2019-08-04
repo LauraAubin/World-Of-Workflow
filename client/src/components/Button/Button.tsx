f0cced9afb71bfed5e31177ad135d44eef2b770e
Laura Aubin <laura.aubin@shopify.com>
2019-08-04 00:58:44 -0400

    Add the minimize option to the Button component

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -5,33 +5,48 @@ import Image from '../Image';
 import Button from '../../art/Buttons/Button.png';
 import ButtonHover from '../../art/Buttons/ButtonHover.png';
 import ButtonPressed from '../../art/Buttons/ButtonPressed.png';
+import Minimize from '../../art/Buttons/Minimize.png';
 
 import './Button.scss';
 
 interface Props {
+  children?: string;
+  minimize?: boolean;
   width?: number;
+  height?: number;
   onClick(): void;
-  children: string;
 }
 
-export function Btn({ width = 100, onClick, children }: Props) {
-  const [image, setImage] = useState(Button);
+export function Btn({
+  minimize,
+  width = 100,
+  height = 25,
+  onClick,
+  children
+}: Props) {
+  const defaultImage = (minimize && Minimize) || Button;
+
+  const [image, setImage] = useState(defaultImage);
+
+  const overlayMarkup = children
+    ? {
+        element: <span>{children}</span>,
+        y: 8
+      }
+    : undefined;
 
   return (
     <button
       onClick={onClick}
-      onMouseEnter={() => setImage(ButtonHover)}
-      onMouseDown={() => setImage(ButtonPressed)}
-      onMouseUp={() => setImage(Button)}
-      onMouseLeave={() => setImage(Button)}
+      onMouseEnter={() => setImage((minimize && Minimize) || ButtonHover)}
+      onMouseDown={() => setImage((minimize && Minimize) || ButtonPressed)}
+      onMouseUp={() => setImage((minimize && Minimize) || Button)}
+      onMouseLeave={() => setImage((minimize && Minimize) || Button)}
       className='Button'
     >
       <Image
-        content={{ element: image, height: 25, width }}
-        overlay={{
-          element: <span>{children}</span>,
-          y: 8
-        }}
+        content={{ element: image, width, height }}
+        overlay={overlayMarkup}
       />
     </button>
   );
