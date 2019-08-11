e115356d6e69fa4c62c2acdfabf2daa0eb132034
Laura Aubin <laura.aubin@shopify.com>
2019-08-10 22:03:16 -0400

    Remove use of Image from the QuestList icon

--- client/src/components/pageElements/QuestList/components/Quest/Quest.tsx
+++ client/src/components/pageElements/QuestList/components/Quest/Quest.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import { Quest as QuestType } from '../../../../../types';
 
-import Image from '../../../../universalElements/Image';
-
-import QuestIconBlank from '../../../../../art/QuestList/QuestIconBlank.png';
-import QuestionMark from '../../../../../art/QuestList/QuestionMark.png';
-
 import './Quest.scss';
 
 interface Props {
@@ -21,14 +16,13 @@ export default function Quest({ record, setSelectedQuest }: Props) {
       key={record.created_at}
       onClick={() => setSelectedQuest(record)}
     >
-      <Image
-        content={{ element: QuestIconBlank, height: 22 }}
-        overlay={{ element: QuestionMark, height: 11, x: 7, y: 1 }}
-      />
+      <div className='questIcon'>
+        <div className='questIconSymbol' />
+      </div>
 
-      <div className='questDetails'>
-        <div className="title">{record.title}</div>
-        <div className="objective">- {record.questObjectives}</div>
+      <div className='questDetailsText'>
+        <div className='title'>{record.title}</div>
+        <div className='objective'>- {record.questObjectives}</div>
       </div>
     </div>
   );
--- client/src/components/pageElements/QuestList/components/QuestSection/QuestSection.tsx
+++ client/src/components/pageElements/QuestList/components/QuestSection/QuestSection.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 import { Quest as QuestType } from '../../../../../types';
 
-import Image from '../../../../universalElements/Image';
 import Quest from '../Quest';
-
-import Frame from '../../../../../art/QuestList/Frame.png';
 
 import './QuestSection.scss';
 
