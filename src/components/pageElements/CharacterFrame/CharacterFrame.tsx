a268ddd1201398c6f1311624bd1040ad9188f6f5
Laura Aubin <laura.aubin@shopify.com>
2019-10-09 16:49:39 -0400

    Create a default profile photo

--- src/components/pageElements/CharacterFrame/CharacterFrame.tsx
+++ src/components/pageElements/CharacterFrame/CharacterFrame.tsx
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 
 import './CharacterFrame.scss';
 
+const MY_GRAVATOR =
+  'https://s.gravatar.com/avatar/4dba733acf88e16eed06e2dfae2baf3a?s=80';
+
 interface Props {
   name?: string;
 }
@@ -18,7 +21,7 @@ export default function CharacterFrame({ name = 'Laura' }: Props) {
 
   return (
     <div className='portraitFrame' id='mainElement'>
-      <div className={gravatarStyles} />
+      <img src={MY_GRAVATOR} className={gravatarStyles} />
       <div className='name'>{name}</div>
       <div className='iconFrame'>
         <div className='iconResting' />
