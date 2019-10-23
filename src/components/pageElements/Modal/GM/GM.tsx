44575d15bc633180d505ae2123001291661baf92
Laura Aubin <laura.aubin@shopify.com>
2019-10-23 01:55:48 -0400

    Add a close button to the GM modal

--- src/components/pageElements/Modal/GM/GM.tsx
+++ src/components/pageElements/Modal/GM/GM.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CreateQuest from './CreateQuest';
+import Button from '../../../universalElements/Button';
 
 import './GM.scss';
 
@@ -11,9 +12,14 @@ interface Props {
 export default function GM({ closeModal }: Props) {
   return (
     <div className='container' id='mainElement'>
+      <div className='gmCloseButton'>
+        <Button minimize onClick={closeModal} />
+      </div>
+
       <div className='options'>
         <button>Create Quest</button>
       </div>
+
       <div className='selectedOptionContainer'>
         <CreateQuest closeModal={closeModal} />
       </div>
