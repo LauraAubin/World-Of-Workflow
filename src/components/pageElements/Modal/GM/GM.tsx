fde5e67a5862f4c988ed4e0ac453fa9ee7d1d085
Laura Aubin <laura.aubin@shopify.com>
2019-11-29 16:59:32 -0500

    Add title to GM window

--- src/components/pageElements/Modal/GM/GM.tsx
+++ src/components/pageElements/Modal/GM/GM.tsx
@@ -5,6 +5,7 @@ import { ModalContext } from '../../../../context/modal';
 import Tabs from './Tabs';
 import CreateQuest from './CreateQuest';
 import Button from '../../../universalElements/Button';
+import Typography from '../../../universalElements/Typography';
 
 import './GM.scss';
 
@@ -26,6 +27,10 @@ export default function GM() {
 
   return (
     <div className='container' id='mainElement'>
+      <div className='gmTitle'>
+        <Typography type='content'>Game Master</Typography>
+      </div>
+
       <div className='gmCloseButton'>
         <Button minimize onClick={() => modalContext.onChange(undefined)} />
       </div>
@@ -34,7 +39,7 @@ export default function GM() {
         tabs={[
           { title: 'Create Quest', icon: '', content: <CreateQuest /> },
           {
-            title: 'Admin settings',
+            title: 'Admin Settings',
             icon: '',
             content: <button onClick={dumpTestData}>Dump all test data</button>
           }
